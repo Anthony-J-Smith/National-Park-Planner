04a43311373b9003b8d508f1e144a33589c72b1c
Ryan Zumbrunnen <sirmacinator@gmail.com>
2020-03-19 12:51:09 -0500

    appending info to the page

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
 // NPS API
-var queryURL = "https://developer.nps.gov/api/v1/parks?parkCode=grpo&api_key=cqjOFJchUaVcTbkS5D8SYpZ1Mfz1wQciEmZ3P5Cf";
+
+var stateIn = "MN"
+var queryURL = "https://developer.nps.gov/api/v1/parks?parkCode=&stateCode=" + stateIn + "&api_key=cqjOFJchUaVcTbkS5D8SYpZ1Mfz1wQciEmZ3P5Cf";
 $.ajax({
     url: queryURL,
     method: "GET"
 }).then(function(response) {
-      console.log(response.data[0].name);
-    });
+    //   console.log(response);
+      // render name, description
+      console.log(response.data[0].name)
+      console.log(response.data[0].description)
+
+    // Not able to grab specific objects (name, description)
+    //   var { name, description } = response;
+
+    // left $() blank
+    // var movieDiv = $("#park-name");
+    var name = response.data[0].name;
+    var parkName = $("<p>").text("Name: " + name);
+    //var parkDes = $().text(data[0].description);
+
+    //append text to $() html or create tags using jquery method.
+    $("#park-name").append(parkName);
+    
+      
+});
+
+
 
 // Weather API
 var cityState = "Minneapolis,MN"
