59fd8b08e8dbafb3f316e37c31b3547c99800e0d
Ryan Zumbrunnen <sirmacinator@gmail.com>
2020-03-19 13:23:22 -0500

    appending info to card

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 // NPS API
-
-var stateIn = "MN"
+var stateIn = "IA"
 var queryURL = "https://developer.nps.gov/api/v1/parks?parkCode=&stateCode=" + stateIn + "&api_key=cqjOFJchUaVcTbkS5D8SYpZ1Mfz1wQciEmZ3P5Cf";
 $.ajax({
     url: queryURL,
@@ -8,26 +7,24 @@ $.ajax({
 }).then(function(response) {
     //   console.log(response);
       // render name, description
-      console.log(response.data[0].name)
-      console.log(response.data[0].description)
-
+      //console.log(response.data[0].name)
+      //console.log(response.data[0].description)
+      console.log(response.data[0].images[0].url)
     // Not able to grab specific objects (name, description)
     //   var { name, description } = response;
-
     // left $() blank
     // var movieDiv = $("#park-name");
     var name = response.data[0].name;
-    var parkName = $("<p>").text("Name: " + name);
-    //var parkDes = $().text(data[0].description);
-
+    var des = response.data[0].description
+    var img = response.data[0].images[0].url
+    var parkName = $("h5").text("Name: " + name);
+    var parkDes = $("#placeholder").text("Description: " + des);
+    var parkImg = $("img").attr("src" , img)
     //append text to $() html or create tags using jquery method.
-    $("#park-name").append(parkName);
-    
-      
-});
-
-
-
+    $(".card-title").append(parkName);
+    $("#placeholder").append(parkDes);
+    $("#img").append(parkImg)
+    });
 // Weather API
 var cityState = "Minneapolis,MN"
 var queryURLweather = "http://api.openweathermap.org/data/2.5/forecast?q=" + cityState + ",US&appid=c8964eed8811dd75cc40e8ea60039298";
